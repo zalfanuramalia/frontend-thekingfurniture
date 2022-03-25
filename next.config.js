756ca8cede995a4827a782b59e7dbc2f7314d999
eexxprd <aviantofan@gmail.com>
2022-03-25 18:34:25 +0700

    feat: updates

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_BACKEND_URL: 'http://localhost:5000',
@@ -9,13 +8,6 @@ const nextConfig = {
     // loader: "cloudinary",
     // path: "https://res.cloudinary.com",
     domains: ['res.cloudinary.com']
-=======
-  // reactStrictMode: true,
-  images: {
-    // louder:"cloudinary",
-    // path:"https://res.cloudinary.com",
-    domains:['res.cloudinary.com'],
->>>>>>> 6117b64bd840cbf915f51672c7e6eeb298517fe5
   }
 }
 
--- redux/reducers/pages.js
+++ redux/reducers/pages.js
@@ -1,5 +1,4 @@
 const initialState = {
-<<<<<<< HEAD
   isLoading: false
 }
 
@@ -15,23 +14,6 @@ const pages = (state = initialState, action) => {
       return { ...state }
     }
   }
-=======
-    isLoading: false
-}
-
-const pages = (state = initialState, action) => {
-    switch (action.type) {        
-        case 'TOGGLE_LOADING': {
-            let {isLoading} = state
-            isLoading = !isLoading
-            state.isLoading = true
-            return {...state }
-        }
-        default: {
-            return {...state }
-        }
-    }
->>>>>>> 6117b64bd840cbf915f51672c7e6eeb298517fe5
 }
 
 export default pages
